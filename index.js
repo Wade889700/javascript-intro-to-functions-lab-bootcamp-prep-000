b7d3d3ccf1366b7d7e3c206650bcb9c84eff0c09
severus889700 <severus889700@gmail.com>
2018-09-30 20:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ function sayHiToGrandma(string){
     return "I love you, too."
   }
 }
-logshout(){
-  
+logshout(string){
+  console.log(string)
 }
+logWhisper(string){
+  console.log(string)
+}
+/*describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
+
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})*/
