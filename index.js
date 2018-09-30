fad85ab95b570270c0b0f9b22554032adc69397a
severus889700 <severus889700@gmail.com>
2018-09-30 20:29:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,5 @@ function sayHiToGrandma(string){
     return "I love you, too."
   }}
 function logShout(string){
-  if(string.toUpperCase()===toHaveBeenCalledWith('HELLO')){
-    console.log(string)
-  }
+  return console.log(string.toUpperCase())
 }
