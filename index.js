48ddb7156844dcba9c6824cc614478a8e2a9b30d
severus889700 <severus889700@gmail.com>
2018-09-30 20:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,26 +21,3 @@ logshout(string){
 logWhisper(string){
   console.log(string)
 }
-/*describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
-
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})*/
